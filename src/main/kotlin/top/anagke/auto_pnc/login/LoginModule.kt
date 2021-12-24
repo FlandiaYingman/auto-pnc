ed9f9774fa2e6e41fa0c9975223de9c5b50bf23a
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-12-25 00:06:23 +0800

    feat: login module

---
+++ src/main/kotlin/top/anagke/auto_pnc/login/LoginModule.kt
@@ -0,0 +1,48 @@
+package top.anagke.auto_pnc.login
+
+import top.anagke.auto_android.AndroidActivity
+import top.anagke.auto_android.AutoModule
+import top.anagke.auto_android.Device
+import top.anagke.auto_android.await
+import top.anagke.auto_android.find
+import top.anagke.auto_android.img.Tmpl
+import top.anagke.auto_android.nap
+import top.anagke.auto_android.whileNotMatch
+import top.anagke.auto_pnc.template
+import top.anagke.auto_pnc.主界面
+
+class LoginModule(
+    private val device: Device,
+) : AutoModule {
+
+    companion object {
+        private val PNC_ACTIVITY = AndroidActivity(
+            "com.sunborn.neuralcloud.cn",
+            "com.mica.micasdk.ui.FoundationActivity",
+        )
+
+        private val 登录界面: Tmpl = template("登录界面.png")
+        private val 主界面_可退出: Tmpl = template("主界面_可退出.png", diff = 0.15)
+    }
+
+    override fun run() {
+        launchPnc()
+        login()
+    }
+
+    private fun launchPnc() {
+        device.stop(PNC_ACTIVITY)
+        device.launch(PNC_ACTIVITY)
+    }
+
+    private fun login() {
+        device.await(登录界面)
+        device.tap(640, 600) //开始游戏
+
+        device.whileNotMatch(主界面) {
+            val pos = device.find(主界面_可退出)
+            if (pos != null) device.tap(pos.x, pos.y).nap()
+        }
+    }
+
+}
